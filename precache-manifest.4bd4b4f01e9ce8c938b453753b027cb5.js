0a34c1149b4b246e94f74fe17f1260b97cacd1e6
masoudNemati <masoud.nnr@gmail.com>
2020-07-05 16:47:55 +0430

    Updates

---
+++ precache-manifest.4bd4b4f01e9ce8c938b453753b027cb5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ae1b72c35b2d73c345db097f8a60d572",
+    "url": "/eminrasen-ir/index.html"
+  },
+  {
+    "revision": "daa51602b1e90a82661d",
+    "url": "/eminrasen-ir/static/css/main.604e080b.chunk.css"
+  },
+  {
+    "revision": "4bf9b5a86519d9379ec1",
+    "url": "/eminrasen-ir/static/js/2.451c2867.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/eminrasen-ir/static/js/2.451c2867.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "daa51602b1e90a82661d",
+    "url": "/eminrasen-ir/static/js/main.06ad44e8.chunk.js"
+  },
+  {
+    "revision": "4b8463d01eaaf1b003cd",
+    "url": "/eminrasen-ir/static/js/runtime-main.2487510c.js"
+  },
+  {
+    "revision": "bbd0e8812fc1a3c9691096085728ad4b",
+    "url": "/eminrasen-ir/static/media/eminrasenhero.bbd0e881.jpg"
+  }
+]);
